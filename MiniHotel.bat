26ea5bc3447aaabd38c9a933256205fe6bd1f2e9
LostSoul <miro.slezak0601@gmail.com>
2026-04-01 11:00:08 +0200

    Update MiniHotel.bat

--- MiniHotel.bat
+++ MiniHotel.bat
@@ -1,8 +1,8 @@
 @echo off
 
-cd MiniHotel-frontend
+cd frontend
 start cmd /k "npm i --legacy-peer-deps & npm run dev"
 
 cd ..
-cd MiniHotel-backend
-start cmd /k "pip install -r requirements.txt & python main.py"
+cd backend
+start cmd /k "if not exist venv (python -m venv venv) & venv\Scripts\activate & pip install -r requirements.txt & python main.py"
